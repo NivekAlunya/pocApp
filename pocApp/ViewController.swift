55feb9c2e376e69b7155618943f1920eafd242ee
Kevin Launay <klaunay.private@gmail.com>
2016-04-20 21:22:49 +0200

    Initial Commit

---
+++ pocApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pocApp
+//
+//  Created by Kevin Launay on 4/20/16.
+//  Copyright © 2016 Kevin Launay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
